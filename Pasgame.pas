c794d85303689c20142bc4cc5505fd03a78a8d41
ProtoCloud <claudiogabrielsmoura@hotmail.com>
2018-03-16 22:34:31 -0300

    pasgame foi hein

---
+++ Pasgame.pas
@@ -0,0 +1,11 @@
+Program Pzim ;
+var jogo:string;
+Begin
+write('Voce curte Dota ou Lol');
+readln(jogo);
+if(jogo='Dota')
+then write('Vai jogar smite');
+if(jogo='LoL')
+then write('Voce deveria curtir Dota');
+   
+End.
